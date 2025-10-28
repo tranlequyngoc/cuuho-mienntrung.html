8fa107f9d55b65d214b563161b2b55884872638a
tranlequyngoc <tranlequyngoc.2008tt@gmail.com>
2025-10-29 01:24:57 +0700

    Implement service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'cuuho-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/assets/logo.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
